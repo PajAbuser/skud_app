24b5bc570526574a2f8fc536ef346138d3e7b4e2
Paj abuser <philippstriz@mail.ru>
2024-04-27 14:52:21 +0300

    Trying fixes for pytest

---
+++ skud_app/tests/__init__.py
@@ -0,0 +1,4 @@
+import os
+
+
+os.environ.update({'DJANGO_SETTINGS_MODULE': '\skud\skud_app\skud\settings.py'}) 
---
+++ skud_app/tests/init.py
@@ -0,0 +1,6 @@
+import os
+import sys
+
+
+# sys.path.append(os.getcwd())
+sys.path.append("D:\Projects\AoIaD\skud\skud_app\skud_app")
